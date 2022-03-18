05f1f253e2bef419e2e3249742e4169e74307319
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-03-18 19:10:52 +0100

    Create GridItem.tsx

---
+++ src/components/GridItem/GridItem.tsx
@@ -0,0 +1,17 @@
+import {Level} from '../../helpers/bmi';
+import styles from './GridItem.module.css';
+import upImage from '../../assets/up.png';
+import downImage from '../../assets/down.png';
+
+// type Props = {
+//     item: Level,
+// }
+
+export const GridItem = () => {
+    return (
+        <div className={styles.main} >
+            {/* style=backgroundColor: {item.color} */}
+        
+        </div>
+    );
+}
